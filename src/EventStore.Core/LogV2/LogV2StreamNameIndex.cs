bb4bd753b1a1ea013aaa9f7944846be0946947d5
shaan1337 <sniper111@gmail.com>
2021-05-05 10:09:42 +0400

    Ensure streamName is not null or empty in implementations of `bool GetOrAddId(string streamName, out TStreamId streamId)`

--- src/EventStore.Core/LogV2/LogV2StreamNameIndex.cs
+++ src/EventStore.Core/LogV2/LogV2StreamNameIndex.cs
@@ -1,4 +1,5 @@
-﻿using EventStore.Core.LogAbstraction;
+﻿using EventStore.Common.Utils;
+using EventStore.Core.LogAbstraction;
 
 namespace EventStore.Core.LogV2 {
 	public class LogV2StreamNameIndex :
@@ -10,6 +11,7 @@ namespace EventStore.Core.LogV2 {
 		}
 
 		public bool GetOrAddId(string streamName, out string streamId) {
+			Ensure.NotNullOrEmpty(streamName, "streamName");
 			streamId = streamName;
 			return true;
 		}
--- src/EventStore.Core/LogV3/StreamNameIndexMetastreamDecorator.cs
+++ src/EventStore.Core/LogV3/StreamNameIndexMetastreamDecorator.cs
@@ -1,4 +1,5 @@
-﻿using EventStore.Core.LogAbstraction;
+﻿using EventStore.Common.Utils;
+using EventStore.Core.LogAbstraction;
 using EventStore.Core.Services;
 
 namespace EventStore.Core.LogV3 {
@@ -16,6 +17,7 @@ namespace EventStore.Core.LogV3 {
 		}
 
 		public bool GetOrAddId(string streamName, out long streamId) {
+			Ensure.NotNullOrEmpty(streamName, "streamName");
 			if (SystemStreams.IsMetastream(streamName)) {
 				streamName = SystemStreams.OriginalStreamOf(streamName);
 				var ret = GetOrAddId(streamName, out streamId);
